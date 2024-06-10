3675fee319d8708238b5ba05d0d477d0b0c9ce06
GouravSoni9068 <gouravsoni9068@gmail.com>
2024-06-11 00:36:30 +0530

    make a frame now add js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector(".Submitbtn").addEventListener("click",(e)=>{
+    // e.preventDefault();
+    console.log('click');
+    // console.log(username.value, password.value)
+    
+})
